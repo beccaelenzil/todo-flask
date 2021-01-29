e90340432b09084d21f0cb7c6e217f10f62a9e4f
Becca <becca@adadevelopersacademy.org>
2021-01-29 13:18:25 -0800

    cors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_seeder import FlaskSeeder
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 db.init_app(app)
